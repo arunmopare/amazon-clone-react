d3d8a4b57e697025aebe69f87f34de74534ae46a
Arun Mopare <arunmopare546@gmail.com>
2021-09-16 23:47:23 +0530

    added realtime expenses list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import Expense from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-const App = () => {
-  const expenses = [
-    {
-      id: "1",
-      title: "Car Insurance",
-      amount: 55,
-      date: new Date(10, 6, 2020),
-    },
-    {
-      id: "e2",
-      title: "Medical Bill",
-      amount: 75,
-      date: new Date(13, 6, 2020),
-    },
-    {
-      id: "e3",
-      title: "Mobile Purchase",
-      amount: 54,
-      date: new Date(11, 6, 2020),
-    },
-    { id: "e4", title: "Shopping", amount: 81, date: new Date(14, 6, 2020) },
-  ];
+import { useState } from "react";
 
-  const addExpenseHandler = (expenses) => {
-    console.log("In app.js");
-    console.log(expenses);
+const DUMMY_EXPENSES = [
+  {
+    id: "1",
+    title: "Car Insurance",
+    amount: 55,
+    date: new Date(10, 6, 2020),
+  },
+  {
+    id: "e2",
+    title: "Medical Bill",
+    amount: 75,
+    date: new Date(13, 6, 2020),
+  },
+  {
+    id: "e3",
+    title: "Mobile Purchase",
+    amount: 54,
+    date: new Date(11, 6, 2020),
+  },
+  { id: "e4", title: "Shopping", amount: 81, date: new Date(14, 6, 2020) },
+];
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = (expense) => {
+    setExpenses((oldExpenses) => {
+      return [expense, ...oldExpenses];
+    });
   };
 
   return (
